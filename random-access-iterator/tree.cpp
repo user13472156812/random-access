812215232d2908192ca55969a774ed2fbdd4f247
Alexey Kreshchuk <akrsch@gmail.com>
2021-06-01 01:25:48 +0300

    clang-format

--- random-access-iterator/tree.cpp
+++ random-access-iterator/tree.cpp
@@ -1,11 +1,11 @@
 #include "tree.hpp"
 
 tree::iterator operator+(tree::iterator const &self, ptrdiff_t diff) {
-  return self;
+    return self;
 }
 
 ptrdiff_t operator-(tree::iterator const &self, tree::iterator other) {
-  return 0;
+    return 0;
 }
 
 auto tree::begin() const -> iterator { return iterator{root.get()}; }
